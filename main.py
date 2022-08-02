fead74f69733356604574944507b851cd71909a1
Renato Rebouças <renato190698@gmail.com>
2022-08-02 19:27:12 -0300

    first commit
    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QGridLayout
+
+class App(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.cw = QWidget()
+        self.grid = QGridLayout(self.cw)
+
+        self.btn = QPushButton('Texto do btn')
+        self.btn.setStyleSheet('font-size: 40px;')
+        self.grid.addWidget(self.btn, 0,0,1,1)
+        # self.btn.clicked.connect(lambda: print('clicou btn!'))
+        self.btn.clicked.connect(self.acao)
+        self.setCentralWidget((self.cw))
+
+    def acao(self):
+        print('clicou btn!')
+if __name__ == '__main__':
+    qt = QApplication(sys.argv)
+    app = App()
+    app.show()
+    qt.exec_()
